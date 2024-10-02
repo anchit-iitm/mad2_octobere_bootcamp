17bbbb57aed8693c7e508e312fb6da5427eb3407
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2024-10-02 19:36:35 +0530

    modified:   mad1_ver/app.py 	new file:   mad1_ver/templates/index.html 	new file:   mad1_ver/templates/index1.html

--- mad1_ver/app.py
+++ mad1_ver/app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/helloworld')
+def hello_world():
+    return 'Hello, World! from anchit'
+
+@app.route('/test')
+def test():
+    return render_template('index.html')
+
+@app.route('/test1')
+def test1():
+    msg = 'hi campers'
+    var2 = False
+    return render_template('index1.html', var1=msg, var3=var2)
 
 if __name__ == '__main__':
     app.run(debug=True)
