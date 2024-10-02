1369005580fbd0f6554655c597c2b02b3a1b99e4
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2024-10-02 20:07:16 +0530

    modified:   mad1_ver/app.py 	modified:   mad1_ver/templates/index1.html 	new file:   mad1_ver/templates/index2.html

--- mad1_ver/app.py
+++ mad1_ver/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -11,12 +11,29 @@ def test():
     return render_template('index.html')
 
 
-@app.route('/test1')
+@app.route('/test12345')
 def test1():
     msg = 'hi campers'
     var2 = True
     var4 = ['iitm', 'iitkgp', 'iitb', 'iitd']
     return render_template('index1.html', var1=msg, var3=var2, var5=var4)
+
+@app.route('/test2', methods=['GET', 'POST'])
+def test2():
+    if request.method == "POST":
+        name_var = request.form.get('name')
+        print(name_var)
+        return render_template('index2.html')
+    # if request.method == "GET":
+    #     return render_template('index2.html')
+    return render_template('index2.html')
+
+@app.route('/test3', methods=['POST'])
+def test3():
+    name_var = request.form.get('name')
+    print(name_var)
+    # return redirect('/test1')
+    return redirect(url_for('test1'))
 
 if __name__ == '__main__':
     app.run(debug=True)
